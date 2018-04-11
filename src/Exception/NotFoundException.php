142c5cc5fe2b95cacc35b71686085407ba0454d3
ularanigu <38263557+ularanigu@users.noreply.github.com>
2018-04-11 11:43:32 -0400

    Add the not found exception.

---
+++ src/Exception/NotFoundException.php
@@ -0,0 +1,23 @@
+<?php
+declare(strict_types=1);
+/**
+ * Fusion Dependency Injector.
+ * A PSR-11 compliant dependency injector.
+ *
+ * @license <https://github.com/ularanigu/fusion/blob/master/LICENSE>.
+ * @link    <https://github.com/ularanigu/fusion>.
+ */
+
+namespace Ularanigu\Fusion\Exception;
+
+use Psr\Container\NotFoundExceptionInterface;
+use UnexpectedValueException;
+
+/**
+ * @link  <https://secure.php.net/manual/en/class.unexpectedvalueexception.php>.
+ * @link  <https://github.com/php-fig/fig-standards/blob/master/accepted/PSR-11-container.md>.
+ * @class NotFoundException.
+ */
+class NotFoundException extends UnexpectedValueException implements NotFoundExceptionInterface
+{
+}
