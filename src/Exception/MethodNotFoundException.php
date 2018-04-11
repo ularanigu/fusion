bd10d040e0b6723fde8fa916376fab026f9761a6
ularanigu <38263557+ularanigu@users.noreply.github.com>
2018-04-11 11:50:16 -0400

    Add the method not found exception.

---
+++ src/Exception/MethodNotFoundException.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+/**
+ * Fusion Dependency Injector.
+ * A PSR-11 compliant dependency injector.
+ *
+ * @license <https://github.com/ularanigu/fusion/blob/master/LICENSE>.
+ * @link    <https://github.com/ularanigu/fusion>.
+ */
+
+namespace Ularanigu\Fusion\Exception;
+
+/**
+ * NotFoundException.
+ */
+class MethodNotFoundException extends NotFoundException
+{
+}
