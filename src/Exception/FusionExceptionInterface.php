2c5419ba2fec2b6f00d7fe9de4130f453c85d6f5
ularanigu <38263557+ularanigu@users.noreply.github.com>
2018-04-11 11:47:19 -0400

    Add our own fusion exception interface.

---
+++ src/Exception/FusionExceptionInterface.php
@@ -0,0 +1,22 @@
+<?php
+declare(strict_types=1);
+/**
+ * Fusion Dependency Injector.
+ * A PSR-11 compliant dependency injector.
+ *
+ * @license <https://github.com/ularanigu/fusion/blob/master/LICENSE>.
+ * @link    <https://github.com/ularanigu/fusion>.
+ */
+ 
+namespace Ularanigu\Fusion\Exception;
+
+use Throwable;
+
+/**
+ * @link <https://secure.php.net/manual/en/class.throwable.php>.
+ * 
+ * FusionExceptionInterface.
+ */
+interface FusionExceptionInterface extends Throwable
+{
+}
