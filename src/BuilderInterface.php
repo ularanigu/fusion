26403f24c32238ccf8da4b81d3bdf267b708266b
Nicholas English <38263557+ularanigu@users.noreply.github.com>
2018-04-13 22:26:41 -0400

    Add the builder interface.

--- src/BuilderInterface.php
+++ src/BuilderInterface.php
@@ -1,1 +1,30 @@
+<?php
+declare(strict_types=1);
+/**
+ * Fusion Dependency Injector.
+ * A PSR-11 compliant dependency injector.
+ *
+ * @license <https://github.com/ularanigu/fusion/blob/master/LICENSE>.
+ * @link    <https://github.com/ularanigu/fusion>.
+ */
 
+namespace Ularanigu\Fusion;
+
+/**
+ * BuilderInterface.
+ */
+interface BuilderInterface
+{
+ 
+    /**
+     * Builder constructor.
+     *
+     * @param array[] $keysToInject Some keys to inject off the bat.
+     *
+     * NOTE: If objects are injected through constructors you will not be able to inject parameters in
+     * the object you just injected.
+     * 
+     * @return void Return nothing.
+     */
+    public function __construct(array $keysToInject = array());
+}
