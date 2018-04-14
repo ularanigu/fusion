35f24c3ad8dd299249a8395f57c72794274063f3
ularanigu <38263557+ularanigu@users.noreply.github.com>
2018-04-13 20:39:08 -0400

    Add function.

---
+++ boot.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+/**
+ * Fusion Dependency Injector.
+ * A PSR-11 compliant dependency injector.
+ *
+ * @license <https://github.com/ularanigu/fusion/blob/master/LICENSE>.
+ * @link    <https://github.com/ularanigu/fusion>.
+ */
+
+if (!function_exists('array_depth')) {
+    function (array $array): int
+    {
+    }
+}
