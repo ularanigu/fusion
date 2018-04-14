dc40d1440ac1a5649eb4cee1c4947a883a9bb369
ularanigu <38263557+ularanigu@users.noreply.github.com>
2018-04-13 21:21:15 -0400

    Update Builder.php

--- src/Builder.php
+++ src/Builder.php
@@ -1,1 +1,38 @@
+<?php
+declare(strict_types=1);
+/**
+ * Fusion Dependency Injector.
+ * A PSR-11 compliant dependency injector.
+ *
+ * @license <https://github.com/ularanigu/fusion/blob/master/LICENSE>.
+ * @link    <https://github.com/ularanigu/fusion>.
+ */
 
+namespace Ularanigu\Fusion;
+
+/**
+ * Builder.
+ */
+class Builder extends KeyHolder implements BuilderInterface
+{
+
+    /**
+     * Builder constructor.
+     *
+     * @param array $keysToInject Some keys to inject off the bat.
+     *
+     * NOTE: If objects are injected through constructors you will not be able to inject parameters in
+     * the object you just injected.
+     * 
+     * @return void Return nothing.
+     */
+    public function __construct(array $keysToInject = array())
+    {
+        if (!empty($keysToInject)) {
+            if (\array_depth($keysToInject) !== 1) {
+                throw new InvalidArrayDepthException('The array depth is not 1.');
+            }
+            static::add($keysToInject);
+        }
+    }
+}
