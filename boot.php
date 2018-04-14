428e3165c4fe771e6701077c420be32d7e86c37e
ularanigu <38263557+ularanigu@users.noreply.github.com>
2018-04-13 20:43:24 -0400

    Add ability to get depth of array.

--- boot.php
+++ boot.php
@@ -8,8 +8,33 @@ declare(strict_types=1);
  * @link    <https://github.com/ularanigu/fusion>.
  */
 
-if (!function_exists('array_depth')) {
-    function (array $array): int
-    {
-    }
+function (array $array): int
+{
+    return intval(getArrayMaxDepth($array));
 }
+
+/** @link https://stackoverflow.com/a/19036761/9524661 {{ **/
+/*** IN mixed (any value),OUT (string)maxDepth ***/
+/*** Retorna la profundidad maxima de un array ***/
+function getArrayMaxDepth($input)
+{
+    if (!canVarLoop($input)) {
+        return "0";
+    }
+    $arrayiter = new RecursiveArrayIterator($input);
+    $iteriter = new RecursiveIteratorIterator($arrayiter);
+    foreach ($iteriter as $value) {
+        //getDepth() start is 0, I use 0 for not iterable values
+        $d = $iteriter->getDepth() + 1;
+        $result[] = "$d";
+    }
+    return max($result);
+}
+
+/*** IN mixed (any value),OUT (bool)true/false, CHECK if can be used by foreach ***/
+/*** Revisa si puede ser iterado con foreach ***/
+function canVarLoop($input)
+{
+    return (is_array($input) || $input instanceof Traversable) ? true : false;
+}
+/** }} */
