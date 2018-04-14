18318047d7f3643360e0f9d8c362bf943d90073f
ularanigu <38263557+ularanigu@users.noreply.github.com>
2018-04-13 20:44:14 -0400

    Update boot.php

--- boot.php
+++ boot.php
@@ -14,8 +14,6 @@ function (array $array): int
 }
 
 /** @link https://stackoverflow.com/a/19036761/9524661 {{ **/
-/*** IN mixed (any value),OUT (string)maxDepth ***/
-/*** Retorna la profundidad maxima de un array ***/
 function getArrayMaxDepth($input)
 {
     if (!canVarLoop($input)) {
@@ -24,15 +22,11 @@ function getArrayMaxDepth($input)
     $arrayiter = new RecursiveArrayIterator($input);
     $iteriter = new RecursiveIteratorIterator($arrayiter);
     foreach ($iteriter as $value) {
-        //getDepth() start is 0, I use 0 for not iterable values
         $d = $iteriter->getDepth() + 1;
         $result[] = "$d";
     }
     return max($result);
 }
-
-/*** IN mixed (any value),OUT (bool)true/false, CHECK if can be used by foreach ***/
-/*** Revisa si puede ser iterado con foreach ***/
 function canVarLoop($input)
 {
     return (is_array($input) || $input instanceof Traversable) ? true : false;
